a1ae3af0df28bf194710ef32f4bce4b076b8d482
aichingung <aichingung@gmail.com>
2022-06-21 20:36:04 +0800

    stats bar

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-
+const leftBar = document.querySelector('.left-bar')
 
 let map;
 
@@ -10,4 +10,46 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+
+
+
+axios.get('/api/owners/total').then(res => {
+  let allData = res.data
+  let total = 0
+
+  let title1 = document.createElement('h1')
+  let subHeader = document.createElement('h2')
+  let title2 = document.createElement('h1')
+  let ownersTable = document.createElement('table')
+  let totalOwners = document.createElement('div')
+
+
+  title1.textContent = 'stats'
+  subHeader.textContent = 'total stations'
+  title2.textContent = 'breakdown by owners'
+
+  allData.forEach(data => {
+    
+    let totalCountOfOwners = total += Number(data.count)
+
+    let column = document.createElement('tr')
+    let ownerRow = document.createElement('td')
+    let countRow = document.createElement('td')
+
+    totalOwners.textContent = totalCountOfOwners
+    ownerRow.textContent = data.owner
+    countRow.textContent = data.count
+
+    leftBar.appendChild(title1)
+    leftBar.appendChild(subHeader)
+    leftBar.appendChild(totalOwners)
+    leftBar.appendChild(title2)
+    leftBar.appendChild(ownersTable)
+    ownersTable.appendChild(column)
+    column.appendChild(ownerRow)
+    column.appendChild(countRow)
+  })
+  
+})
 
--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const app = express()
 const { Pool } = require('pg')
 const port = 8080
 
+db = new Pool({
+  user: 'postgres',
+  database: 'test1',
+  password: ''
+})
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) =>{
   res.render('index')
+})
+
+app.get('/api/owners/total', (req, res) => {
+  const sql = `SELECT owner, count(*) FROM testdata GROUP BY owner;`
+  db.query(sql).then(result => res.send(result.rows))
 })
 
 app.listen(port)
