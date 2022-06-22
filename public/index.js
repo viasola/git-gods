b7f89775873ae23c34f0f8f03c809f7f44ffcbce
edthoo <tfthoo@gmail.com>
2022-06-22 14:08:50 +0800

    detect user location

--- public/index.js
+++ public/index.js
@@ -3,71 +3,28 @@ const leftBar = document.querySelector('.left-bar')
 let map, infoWindow;
 
 function initMap() {
-
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function (position) {
-        currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        map.setCenter(initialLocation);
-    });
-}
-  // const currentLocation = { lat: -33.856159, lng: 151.215256 }
-  map = new google.maps.Map(document.getElementById("map"), {
-    // center property tells API where to center the map
-    center: currentLocation,
-    zoom: 13,
-    minZoom: 11,
-  });
-
-  const marker = new google.maps.Marker({
-    position: currentLocation,
-    map: map,
-  })
-
-  infoWindow = new google.maps.InfoWindow();
-
-  const locationButton = document.createElement("button");
-
-  locationButton.textContent = "Pan to Current Location";
-  locationButton.classList.add("custom-map-control-button");
-  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
-  locationButton.addEventListener("click", () => {
-    // Try HTML5 geolocation.
+  // to test set sensor location to anywhere
+  // you can manage locations and add Melbourne lat: -37.8183, lng: 144.9671, timezone: Australia/Melbourne, locale: en-GB
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-
-          infoWindow.setPosition(pos);
-          infoWindow.setContent("Location found.");
-          infoWindow.open(map);
-          map.setCenter(pos);
-        },
-        () => {
-          handleLocationError(true, infoWindow, map.getCenter());
-        }
-      );
-    } else {
-      // Browser doesn't support Geolocation
-      handleLocationError(false, infoWindow, map.getCenter());
-    }
-  });
-}
-
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(
-    browserHasGeolocation
-      ? "Error: The Geolocation service failed."
-      : "Error: Your browser doesn't support geolocation."
-  );
-  infoWindow.open(map);
-}
-
-
-window.initMap = initMap;
+      navigator.geolocation.getCurrentPosition(function (position) {
+        let currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+  
+        map = new google.maps.Map(document.getElementById("map"), {
+          // center property tells API where to center the map
+          center: currentLocation,
+          zoom: 13,
+          minZoom: 11,
+        });
+  
+        const marker = new google.maps.Marker({
+          position: currentLocation,
+          map: map,
+        });
+      })
+    } 
+  }
+  
+  window.initMap = initMap;
 
 
 
