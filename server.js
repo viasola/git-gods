1eefe1c692a4d4efedd890c99cf9c3b6c13f4850
edthoo <tfthoo@gmail.com>
2022-06-22 09:56:53 +0800

    add json for api endpoint

--- server.js
+++ server.js
@@ -5,26 +5,24 @@ const port = 8080
 
 db = new Pool({
   user: 'postgres',
-  database: 'test1',
-  password: ''
+  database: 'git_gods', //change back to test1
+  password: 'edthoo' //remove this
 })
 
 app.use(express.static('public'))
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
   res.render('index')
 })
 
 app.get('/api/owners/total', (req, res) => {
   const sql = `SELECT owner, count(*) FROM testdata GROUP BY owner;`
-  db.query(sql).then(result => res.send(result.rows))
+  db.query(sql).then(dbRes => res.json(dbRes.rows))
 })
 
 app.get('/api/stations/all', (req, res) => {
   let sql = 'SELECT * FROM testdata;'
-  db.query(sql, (req, dbRes) => {
-    res.send(dbRes.rows)
-  })
+  db.query(sql).then(dbRes => res.json(dbRes.rows))
 })
 
 app.listen(port)
